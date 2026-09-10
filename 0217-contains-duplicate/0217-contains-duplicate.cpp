8a99187e5823ecf1c634484bd5a66c4234486d4a
Vartika Mishra <mishravartika005@gmail.com>
2026-09-10 16:21:44 +0530

    Time: 87 ms (17.42%), Space: 113.6 MB (8.55%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+
+        }
+      for(auto i:mpp){
+        if(i.second >= 2)
+         return true;
+      }
+      return false;
+    }
+};
