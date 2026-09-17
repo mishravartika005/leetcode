28a241e4de39d350dd5400264ad0c52aa6d12af7
Vartika Mishra <mishravartika005@gmail.com>
2026-09-17 05:59:01 +0530

    Time: 0 ms (100%), Space: 22.1 MB (61.7%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,9 +12,11 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        if (!root->left) return 1 + maxDepth(root->right);
-        if (!root->right) return 1 + maxDepth(root->left);
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+       if(root == NULL) return 0;
+       int lh=maxDepth(root->left);
+       int rh=maxDepth(root->right);
+
+       return  1+max(lh,rh);
+         
     }
 };
